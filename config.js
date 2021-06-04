6ac351fac2eaeaf2f5dd1b4ee1d72cba01c5c9bb
Minh Tuấn <64247206+Ruoc1310@users.noreply.github.com>
2021-06-04 09:45:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều',
     messDesc: 'Cuối tuần mình đi cf nhaaaaaaa ^^.',
     btnAccept: 'Okiiiii lun <3',
 }
